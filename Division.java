60196ed70a31ae67257c7858913de86aac3b0851
Yashavantha09 <yashavantha.yashavantha84311@gmail.com>
2026-08-17 18:25:12 +0530

    Added Java programs

---
+++ Division.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class Division {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int result = a / b;
+
+        System.out.println("Division = " + result);
+    }
+}
