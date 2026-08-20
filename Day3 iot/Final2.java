cd5d9cb6f3b696d686f35db81dff8adb9fbca4f5
Yashavantha B N <yashavantha.yashavantha84311@gmail.com>
2026-08-20 08:57:54 +0530

    Add files via upload

---
+++ Day3 iot/Final2.java
@@ -0,0 +1,15 @@
+package Yashugowa;
+
+public class Final2 {
+	public static void main(String[] args) {
+		System.out.println("anand");
+		System.out.println("anand3");
+		try {
+			int a = 10 / 2;
+			System.out.println("kishore");
+		} finally {
+			System.out.println("finally block");
+		}
+		System.out.println("rest of the code");
+	}
+}
